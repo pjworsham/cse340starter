279053408120c51ec5c7b9967130d6f85c57bd0b
pjworsham <145511699+pjworsham@users.noreply.github.com>
2025-09-14 12:31:19 -0400

    w02assignment

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -234,3 +234,18 @@ VALUES (
         'White',
         5
     );
+UPDATE inventory
+SET inv_description = REPLACE (
+        inv_description,
+        'the small interiors',
+        'a huge interior'
+    )
+WHERE inv_make = 'GM' inv_model = 'Hummer';
+
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(
+        inv_thumbnail,
+        '/images/',
+        '/images/vehicles/'
+    );
