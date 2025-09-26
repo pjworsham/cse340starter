4d5e9188358404b6c5dc1882f2baeb8441099238
pjworsham <145511699+pjworsham@users.noreply.github.com>
2025-09-26 17:50:53 -0500

    fix: add spacing for improved readability in server.js

--- server.js
+++ server.js
@@ -65,6 +65,8 @@ app.use("/inv", utilities.handleErrors(inventoryRoute))
 //account routes
 app.use("/account", utilities.handleErrors(accountRoute))
 
+
+
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
